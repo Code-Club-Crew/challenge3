7fbfbb9c7ce6807261d65a1e02a1a561b1b2da02
jagestah <mckeeverjohnj@gmail.com>
2018-10-23 15:26:26 -0600

    Order of Operations

---
+++ jagestah/app.py
@@ -0,0 +1,14 @@
+#!/bin/python
+#order of operations
+    #all arguments need to ignore case
+    #Argument 1 will be the data type such as Starship, Planet, etc
+        #Call the entire data type and save the json
+        #Will need to deal with pagination; can borrow from Challenge 1 alternative
+    #Argument 2 will be a string that is searched for in the argument 1 data type
+        #Add the entire entry to a results list to be printed at the end
+    #Output will be a list of all results found
+        #Print a count of the number of results found
+        #Print a human readable list of the results and their Key-Value pairs
+
+#Maybe add a third argument for the key user is looking for
+    #IE `app.py starships destroyer name` to print the name of anything with a value of destroyer
